f0463587a7da07aaa8f37362a7c1653c5eadcc98
Lucas Duarte <lucas.duartebarbosa@outlook.com>
2022-04-09 22:57:29 -0300

    js em processo

--- javascripts.js
+++ javascripts.js
@@ -1,5 +1,55 @@
-let quantidadeCartas = Number(prompt("Escolha um número par de cartas que deseja começar! (entre 4 a 14)"))
+let papagaios = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+
+let quantidadeCartas = Number(prompt("Escolha um número par de cartas que deseja começar! (entre 4 a 14)"));
+
 
 while(quantidadeCartas % 2 !== 0 || quantidadeCartas > 14 || quantidadeCartas < 4) {
     quantidadeCartas = Number(prompt("Escolha um número par de cartas que deseja começar! (entre 4 a 14)"))
 }
+
+let baralho = []
+
+
+cartasAleatorias();
+aparecerCartas();
+
+/*-----------------------Funções----------------------*/
+
+function cartasAleatorias() {
+ for(let i = 0; i < quantidadeCartas/2; i++){
+     let carta = criarCarta(i);
+     baralho.push(carta);
+     baralho.push(carta);
+ }
+
+ baralho.sort(comparador);
+}
+
+function comparador() {
+    return Math.random() - 0.5;
+}
+
+
+function criarCarta(indiceCarta) {
+  let papagaio = papagaios[indiceCarta]
+    return `
+<div class="cartas">
+       <div class="carta-atras">
+           <img src="./front.png" width="100px" height="100px" alt="papagaio">
+       </div>
+       <div class="carta-frente">
+           <img src="${papagaio}.gif" width="100px" height="100px" alt="${papagaio}">
+       </div>
+        </div>
+`
+}
+
+function aparecerCartas() {
+    let tudao = document.querySelector(".tudao")
+    for(let i = 0; i < baralho.length; i++) {
+        let carta = baralho[i]
+        tudao.innerHTML += carta;
+    }
+}
+
+document.classlist.con
